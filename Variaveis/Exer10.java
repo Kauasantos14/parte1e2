ec3bf655fec546602669a29cf7864b40fecd8f7d
Kauasantos14 <128007858+Kauasantos14@users.noreply.github.com>
2023-07-07 13:57:10 -0300

    Update Exer10.java

--- Variaveis/Exer10.java
+++ Variaveis/Exer10.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class Exer10 {
     public static void main(String[] args) {
 
-
-
         Scanner leia = new Scanner(System.in);
+
+        double X;
+
+        System.out.println("Digite o valor de A: ");
+        double A = leia.nextDouble();
+
+        System.out.println("Digite o valor de B: ");
+        double B = leia.nextDouble();
+
+        X = A;
+        A = B;
+        B = X;
+
+        System.out.println("Agora A vale " + A + " e B vale " + B);
     }
 }
