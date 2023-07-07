9e47c8287b22f26646b79f8d1fda01a556cc480e
Kauasantos14 <128007858+Kauasantos14@users.noreply.github.com>
2023-07-07 14:33:10 -0300

    Update Exer9.java

--- Variaveis/Exer9.java
+++ Variaveis/Exer9.java
@@ -4,36 +4,37 @@ public class Exer9 {
     public static void main(String[] args) {
 
         String rua, cidade, estado, nome;
-        float cpf, cep, telefone, numero;
+        float numero;
+        long cpf, cep, telefone;
 
         Scanner leia = new Scanner(System.in);
 
         System.out.println("Digite seu nome: ");
         nome = leia.nextLine();
 
-        System.out.println("Digite seu endereço começando pela rua: ");
+        System.out.println("Digite o endereço começando pelo seu estado: ");
+        estado = leia.nextLine();
+
+        System.out.println("Agora digite sua rua: ");
         rua = leia.nextLine();
-
-        System.out.println("Agora digite o número de sua casa/apartamento: ");
-        numero = leia.nextFloat();
-
-        System.out.println("Agora digite  seu CEP seguindo o exemplo : ");
-        cep = leia.nextFloat();
 
         System.out.println("Digite sua cidade: ");
         cidade = leia.nextLine();
 
-        System.out.println("Digite seu estado: ");
-        estado = leia.nextLine();
+        System.out.println("Digite o número de sua casa/apartamento: ");
+        numero = leia.nextFloat();
 
-        System.out.println("Digite seu CPF: ");
-        cpf = leia.nextFloat();
+         System.out.println("Digite seu CEP (sem nenhuma pontuação) : ");
+        cep = leia.nextLong(20);
 
-        System.out.println("Digite seu telefone seguindo : ");
-        telefone = leia.nextFloat();
+        System.out.println("Agora digite seus dados pessoais começando pelo seu CPF (sem nenhuma pontuação): ");
+        cpf = leia.nextLong(11);
+
+        System.out.println("Por fim digite seu telefone, seguindo o exemplo (DD000000000) : ");
+        telefone = leia.nextLong(20);
 
         System.out.println("Nome: " + nome);
-        System.out.println("Endereço: RUA" + rua + ", NUMERO: " + numero + ", CEP: " + cep);
+        System.out.println("Endereço: RUA " + rua + ", NUMERO: " + numero + ", CEP: " + cep);
         System.out.println("Cidade: " + cidade);
         System.out.println("Estado: " + estado);
         System.out.println("CPF: " + cpf);
